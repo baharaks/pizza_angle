6cc3d6aa4997c1ef87583f6fc46b18d286a02920
Becky <baharak.soltanian@gmail.com>
2024-02-18 19:03:22 -0800

    0

--- utils.py
+++ utils.py
@@ -8,6 +8,9 @@ Created on Sat Feb 17 16:18:35 2024
 
 import torch
 import numpy as np
+import math
+import random
+import sqlite3
 
 def calculate_angle(x0, y0, x1, y1):
     """Calculate the angle (in degrees) of the vector with respect to the x-axis."""
@@ -28,3 +31,46 @@ def accuracy_metric(predictions, labels, tolerance_degrees=5):
     
     accuracy = correct_predictions / total_predictions
     return accuracy
+
+def read_data(path_database, shuffle_it=True):
+    conn = sqlite3.connect(path_database)
+    
+    # Create a cursor object
+    cursor = conn.cursor()
+    
+    sql_query = """SELECT name FROM sqlite_master  
+      WHERE type='table';"""
+    
+    cursor.execute(sql_query)
+    
+    # print(cursor.fetchall())
+    
+    data_list = []
+    with conn:
+        cursor.execute("SELECT * FROM pizza_table")
+        # print(cursor.fetchall())
+        for res in cursor.fetchall():
+            data_list.append(res)
+    
+    if shuffle_it:
+        random.shuffle(data_list)
+            
+    return data_list
+
+def split_data(data_list):
+    image_list = []
+    label_list = []
+    for data in data_list:
+        _, name, x0, y0, x1, y1 = data
+        image_list.append(name)
+        label_list.append((x0, y0, x1, y1))
+        
+    # split the dataset into 80% train data and 20% test and validation data
+    n = len(image_list)
+    n_train = math.floor(n*0.8)
+    X_train = image_list[0:n_train]  
+    y_train = label_list[0:n_train]
+    X_val = image_list[n_train:n]
+    y_val = label_list[n_train:n]
+    
+    return X_train, y_train, X_val, y_val
